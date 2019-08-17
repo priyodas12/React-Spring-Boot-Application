b56570fb6d9352228501d970da5f10e1680fa3c7
Priyobrato Das <priyodas12@gmail.com>
2019-08-17 15:52:22 +0530

    Create Footer.jsx
    Footer added

---
+++ Footer.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+export class Footer extends Component {
+  render() {
+    return (
+      <footer className="footer">
+        <span className="text-muted">
+          All Copy rights reserved under @Priyobrato
+        </span>
+      </footer>
+    );
+  }
+}
+
+export default Footer;
