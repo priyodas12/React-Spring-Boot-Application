ac0ffb2b158813675207a08a35ba4aa91127bed3
Priyobrato Das <priyodas12@gmail.com>
2019-08-17 15:47:01 +0530

    Update App.js
    modified with authenticated component.

--- App.js
+++ App.js
@@ -3,21 +3,29 @@ import "./App.css";
 import Signin from "./Components/SignIn";
 import Welcome from "./Components/Welcome";
 import Error from "./Components/Error";
+import Footer from "./Components/Footer";
+import Header from "./Components/Header";
 import ToDo from "./Components/ListTodoComponent";
+import Logout from "./Components/Logout";
+import AuthenticatedRoute from "./Components/AuthenticatedRoute";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./bootstrap.css";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <>
+          <Header />
           <Switch>
             <Route path="/" exact component={Signin} />
             <Route path="/login" component={Signin} />
-            <Route path="/todo" component={ToDo} />
-            <Route path="/welcome/:name" component={Welcome} />
+            <AuthenticatedRoute path="/logout" component={Logout} />
+            <AuthenticatedRoute path="/todo" component={ToDo} />
+            <AuthenticatedRoute path="/welcome/:name" component={Welcome} />
             <Route component={Error} />
           </Switch>
+          <Footer />
         </>
       </Router>
     </div>
