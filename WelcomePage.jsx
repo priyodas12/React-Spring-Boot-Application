7057365a86ac03868b530de3ebb969f7a878887e
Priyobrato Das <priyodas12@gmail.com>
2019-08-17 15:49:56 +0530

    Update WelcomePage.jsx
    todo route modified

--- WelcomePage.jsx
+++ WelcomePage.jsx
@@ -1,10 +1,14 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 export class Welcome extends Component {
   render() {
     return (
-      <div>
+      <div className="container">
         <p>Welcome {this.props.match.params.name} to demo site</p>
+        <p>
+          You can manage your todo <Link to="/todo">Here</Link>
+        </p>
       </div>
     );
   }
